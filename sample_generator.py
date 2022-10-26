8ed0b6b09d3e863cc240f839c946ced3ee4d612f
Matthew Leo <matthewleo124@gmail.com>
2022-10-26 14:15:39 +1100

    Added some maybe working restrictions. Added trinkets to sample generator and fixed notation for ASP.

--- sample_generator.py
+++ sample_generator.py
@@ -12,28 +12,31 @@ class Generator():
 
     def generate_row(self):
         data = []
-
-        data.append(self.generate_name())
-        data.append(self.generate_quantity())
-        data.append(self.generate_cost())
-        data.append(self.generate_location())
+        randInt = random.randint(1, 100)
+        print(randInt)
+        if randInt % 20 == 0:
+            data = self.generate_trinket()
+        else:
+            data.append(self.generate_name())
+            data.append(self.generate_quantity())
+            data.append(self.generate_cost())
+            data.append(self.generate_location())
         
-        result = self.name + "(" + ",".join(data) + ")"
+        result = self.name + "(" + ",".join(data) + ")."
         return result
 
     def generate_name(self):
-        names = ["Gear", "Pipe", "Bolt"]
-        types = ["A", "B", "C"]
+        names = ["gear", "pipe", "bolt", "motor", "wire", "chemicals"]
+        types = ["s", "m", "l"]
 
-        result = "'" + names[random.randint(0,len(names) - 1)]
-        result += " "
+        result = names[random.randint(0,len(names) - 1)]
+        result += ","
         result += types[random.randint(0,len(types) - 1)]
-        result += "'"
         return result
 
     def generate_quantity(self):
-        upper = 100
-        lower = 10
+        upper = 10000
+        lower = 0
         return str(random.randint(lower, upper))
 
     def generate_cost(self):
@@ -42,17 +45,20 @@ class Generator():
         return str(random.randint(lower, upper))
 
     def generate_location(self):
-        locations = ["Australia", "USA", "Germany"]
+        locations = ["australia", "usa", "china"]
         result = locations[random.randint(0,len(locations) - 1)]
-        result = "'" + result + "'"
         return result
-        
+
+    def generate_trinket(self):
+        name = random.choice(["thermometer", "phmeter", "flowmeter", "controlpanel"])
+        location = random.choice(["australia", "usa", "switzerland", "japan"])
+        cost = str(random.randint(500, 10000))
+        quantity = str(random.randint(0, 500))
+        return [name, quantity, cost,location]
+
 x = Generator("part")
-f = open("sample_data.txt", "w")
 
-for i in x.generate_data(100):
-    f.write(i)
-    f.write("\n")
-f.close()
-
-
+with open("sample_data.txt", "w") as f:
+    for i in x.generate_data(100):
+        f.write(i)
+        f.write("\n")
